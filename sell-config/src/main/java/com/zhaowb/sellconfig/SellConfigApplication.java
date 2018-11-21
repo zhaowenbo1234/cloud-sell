2e90562cdab6eb147e55db4fd058120758f784dc
济世魔王 <308539393@qq.com>
2018-11-21 14:11:45 +0800

    sell-config  配置中心，

---
+++ sell-config/src/main/java/com/zhaowb/sellconfig/SellConfigApplication.java
@@ -0,0 +1,21 @@
+package com.zhaowb.sellconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created with IDEA
+ *
+ * @author zhaowb
+ * @date 2018/11/21 9:59
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+public class SellConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SellConfigApplication.class,args);
+    }
+}
