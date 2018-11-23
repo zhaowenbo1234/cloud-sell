8a1b2ae4adbed5e3fcda8b6542ed1b0a242f989b
济世魔王 <308539393@qq.com>
2018-11-23 16:49:13 +0800

    商品公共模块为其他模块提供

---
+++ sell-product/sell-product-common/src/main/java/com/zhaowb/sellproductcommon/DecreaseStockInput.java
@@ -0,0 +1,52 @@
+package com.zhaowb.sellproductcommon;
+
+/**
+ * Created with IDEA
+ * 减库存入参
+ * @author zhaowb
+ * @date 2018/11/23 16:39
+ */
+public class DecreaseStockInput {
+
+    /**
+     * 商品id
+     */
+    private String productId;
+
+    /**
+     * 商品数量
+     */
+    private Integer productQuantity;
+
+    public DecreaseStockInput() {
+    }
+
+    public DecreaseStockInput(String productId, Integer productQuantity) {
+        this.productId = productId;
+        this.productQuantity = productQuantity;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public Integer getProductQuantity() {
+        return productQuantity;
+    }
+
+    public void setProductQuantity(Integer productQuantity) {
+        this.productQuantity = productQuantity;
+    }
+
+    @Override
+    public String toString() {
+        return "DecreaseStockInput{" +
+                "productId='" + productId + '\'' +
+                ", productQuantity=" + productQuantity +
+                '}';
+    }
+}
