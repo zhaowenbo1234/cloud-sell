b1fd1edc38b4c54ec8458d6d07a85ea5b17a42a8
济世魔王 <308539393@qq.com>
2018-11-26 14:45:25 +0800

    返回工具类

---
+++ sell-product/sell-product-server/src/main/java/com/zhaowb/sellproduct/util/ResultVOUtil.java
@@ -0,0 +1,20 @@
+package com.zhaowb.sellproduct.util;
+
+import com.zhaowb.sellproduct.vo.ResultVO;
+
+/**
+ * Created with IDEA
+ *  返回封装
+ * @author zhaowb
+ * @date 2018/11/26 14:34
+ */
+public class ResultVOUtil {
+
+    public static ResultVO success(Object object){
+        ResultVO resultVO = new ResultVO();
+        resultVO.setCode(0);
+        resultVO.setData(object);
+        resultVO.setMsg("成功");
+        return resultVO;
+    }
+}
