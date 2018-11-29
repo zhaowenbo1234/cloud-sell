385e9224b86c3ffaedf608841dba4ed254c5c568
济世魔王 <308539393@qq.com>
2018-11-29 19:59:09 +0800

    订单dto

---
+++ sell-order/sell-order-server/src/main/java/com/zhaowb/sellorderserver/dto/OrderDTO.java
@@ -0,0 +1,143 @@
+package com.zhaowb.sellorderserver.dto;
+
+import com.zhaowb.sellorderserver.entity.OrderDetail;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * Created with IDEA
+ *
+ * @author zhaowb
+ * @date 2018/11/29 19:57
+ */
+public class OrderDTO {
+    /** 订单id. */
+    private String orderId;
+
+    /** 买家名字. */
+    private String buyerName;
+
+    /** 买家手机号. */
+    private String buyerPhone;
+
+    /** 买家地址. */
+    private String buyerAddress;
+
+    /** 买家微信Openid. */
+    private String buyerOpenid;
+
+    /** 订单总金额. */
+    private BigDecimal orderAmount;
+
+    /** 订单状态, 默认为0新下单. */
+    private Integer orderStatus;
+
+    /** 支付状态, 默认为0未支付. */
+    private Integer payStatus;
+
+    private List<OrderDetail> orderDetailList;
+
+    public OrderDTO() {
+    }
+
+    public OrderDTO(String orderId, String buyerName, String buyerPhone, String buyerAddress, String buyerOpenid,
+                    BigDecimal orderAmount, Integer orderStatus, Integer payStatus, List<OrderDetail> orderDetailList) {
+        this.orderId = orderId;
+        this.buyerName = buyerName;
+        this.buyerPhone = buyerPhone;
+        this.buyerAddress = buyerAddress;
+        this.buyerOpenid = buyerOpenid;
+        this.orderAmount = orderAmount;
+        this.orderStatus = orderStatus;
+        this.payStatus = payStatus;
+        this.orderDetailList = orderDetailList;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getBuyerName() {
+        return buyerName;
+    }
+
+    public void setBuyerName(String buyerName) {
+        this.buyerName = buyerName;
+    }
+
+    public String getBuyerPhone() {
+        return buyerPhone;
+    }
+
+    public void setBuyerPhone(String buyerPhone) {
+        this.buyerPhone = buyerPhone;
+    }
+
+    public String getBuyerAddress() {
+        return buyerAddress;
+    }
+
+    public void setBuyerAddress(String buyerAddress) {
+        this.buyerAddress = buyerAddress;
+    }
+
+    public String getBuyerOpenid() {
+        return buyerOpenid;
+    }
+
+    public void setBuyerOpenid(String buyerOpenid) {
+        this.buyerOpenid = buyerOpenid;
+    }
+
+    public BigDecimal getOrderAmount() {
+        return orderAmount;
+    }
+
+    public void setOrderAmount(BigDecimal orderAmount) {
+        this.orderAmount = orderAmount;
+    }
+
+    public Integer getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(Integer orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public Integer getPayStatus() {
+        return payStatus;
+    }
+
+    public void setPayStatus(Integer payStatus) {
+        this.payStatus = payStatus;
+    }
+
+    public List<OrderDetail> getOrderDetailList() {
+        return orderDetailList;
+    }
+
+    public void setOrderDetailList(List<OrderDetail> orderDetailList) {
+        this.orderDetailList = orderDetailList;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderDTO{" +
+                "orderId='" + orderId + '\'' +
+                ", buyerName='" + buyerName + '\'' +
+                ", buyerPhone='" + buyerPhone + '\'' +
+                ", buyerAddress='" + buyerAddress + '\'' +
+                ", buyerOpenid='" + buyerOpenid + '\'' +
+                ", orderAmount=" + orderAmount +
+                ", orderStatus=" + orderStatus +
+                ", payStatus=" + payStatus +
+                ", orderDetailList=" + orderDetailList +
+                '}';
+    }
+}
